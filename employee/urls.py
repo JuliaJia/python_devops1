8b627a7e9b2c729315b7c4ad29053677d1e7d729
JuliaJia <juliajia25191@gmail.com>
2022-12-14 09:08:10 +0800

    projects settings

--- employee/urls.py
+++ employee/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import test_index
 urlpatterns = [
-
+    path('',test_index),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,2 +1,15 @@
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse,HttpResponseNotAllowed,Http404,JsonResponse
+from django.views.decorators.http import require_GET,require_POST,require_http_methods
+
+
+
+# @require_GET
+@require_http_methods(['GET','POST'])
 def test_index(request):
-    return 123
+    # return HttpResponse("xyz")
+    return JsonResponse({"1":[1,2,3]},status=204)
+    # if request.method.lower() == 'get':
+    #     return JsonResponse({"1":[1,2,3]},status=204)
+    # else:
+    #     return HttpResponseNotAllowed(["GET"])
