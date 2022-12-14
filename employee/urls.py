9108815085e7ed5c61c1f073190f1f7b7f504ceb
JuliaJia <juliajia25191@gmail.com>
2022-12-15 06:47:31 +0800

    django middleware study

--- employee/urls.py
+++ employee/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import test_index
+from django.views.decorators.http import require_GET
+
+from .views import test_index, TestView
+
 urlpatterns = [
     path('',test_index),
+    path('test/',require_GET(TestView.as_view())),
+    path('test2/',TestView.as_view()),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,7 @@
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse,HttpResponseNotAllowed,Http404,JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.http import require_GET,require_POST,require_http_methods
 
 
@@ -13,3 +15,31 @@ def test_index(request):
     #     return JsonResponse({"1":[1,2,3]},status=204)
     # else:
     #     return HttpResponseNotAllowed(["GET"])
+
+
+class MyMiddleware:
+    def __init__(self,get_response):
+        self.get_response = get_response
+        print(self.__class__.__name__,'init~~~~~~~')
+    def __call__(self,request):
+        print(self.__class__.__name__, 'response之前~~~~~~~')
+        response = self.get_response(request)
+        print(self.__class__.__name__, 'response之后~~~~~~~')
+        return response
+
+
+class TestView(View):
+
+    def dispatch(self,request,*args,**kwargs):
+        return super().dispatch(request,*args,**kwargs)
+
+    def get(self,request):
+        print("我是Get，我被调用了！")
+        return JsonResponse({"2":[4,5,6]},status=200)
+
+
+    def post(self,request):
+        print("我是Post，我被调用了！")
+        return JsonResponse({"3":[7,8,9]},status=200)
+        # return HttpResponseNotAllowed(["GET"])
+
